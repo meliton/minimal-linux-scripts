304b625916a14e4fc9b6c87b55b02c5c37022c8a
Mel <m_hinojosa@rocketmail.com>
2017-10-15 20:22:12 -0500

    Create minimal.sh

---
+++ 2.6.39.4_kernel/minimal.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+# set options: e- exit immediately if a command exits with a non-zero status
+#              x- print commands and their arguments as they are executed 
+set -ex
+
+# Variables for files and start location
+KERNEL_VERSION=2.6.39.4
+BUSYBOX_VERSION=1.27.2
+SYSLINUX_VERSION=6.03
+SRC_DIR=$(pwd)
+
+# Clean old isoimage folder and go into sources folder
+rm -rf isoimage
+mkdir -p sources
+cd $SRC_DIR/sources
+
+# Remove old folders... linux, busybox and syslinux
+rm -rf linux-$KERNEL_VERSION
+rm -rf busybox-$BUSYBOX_VERSION
+rm -rf syslinux-$SYSLINUX_VERSION
+
+# Get source files
+wget -c -O linux-$KERNEL_VERSION.tar.xz http://kernel.org/pub/linux/kernel/v2.6/linux-$KERNEL_VERSION.tar.xz
+wget -c -O busybox-$BUSYBOX_VERSION.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
+wget -c -O syslinux-$SYSLINUX_VERSION.tar.xz http://kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUX_VERSION.tar.xz
+
+# Extract source files, non-verbosely
+echo "Extracting kernel... please wait..."
+tar -xf linux-$KERNEL_VERSION.tar.xz
+echo "Extracting busybox... please wait..."
+tar -xf busybox-$BUSYBOX_VERSION.tar.bz2
+echo "Extracting syslinux... please wait..."
+tar -xf syslinux-$SYSLINUX_VERSION.tar.xz
+
+cd $SRC_DIR
+mkdir -p isoimage
+cd $SRC_DIR/sources/busybox-$BUSYBOX_VERSION
+
+# Configures a full-featured BusyBox w/o debugging
+make distclean defconfig
+
+# Creates a static linked Busybox binary 
+sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
+make busybox install
+
+# Remove the linuxrc shell script file... not needed
+cd _install
+rm -f linuxrc
+
+# Build basic init file and compress the root filesystem
+mkdir dev proc sys
+echo '#!/bin/sh' > init
+echo 'dmesg -n 1' >> init
+echo 'mount -t devtmpfs none /dev' >> init
+echo 'mount -t proc none /proc' >> init
+echo 'mount -t sysfs none /sys' >> init
+echo 'setsid cttyhack /bin/sh' >> init
+chmod +x init
+find . | cpio -R root:root -H newc -o | gzip > $SRC_DIR/isoimage/rootfs.gz
+
+# Configures a default kernel configuration
+cd $SRC_DIR/sources/linux-$KERNEL_VERSION
+make mrproper defconfig 
+
+# Enable CONFIG_DEVTMPFS and CONFIG_DEVTMPFS_MOUNT
+sed -i "s/.*CONFIG_DEVTMPFS.*/CONFIG_DEVTMPFS=y\nCONFIG_DEVTMPFS_MOUNT=y/" .config
+
+make bzImage
+cp arch/x86/boot/bzImage $SRC_DIR/isoimage/kernel.gz
+cd $SRC_DIR/isoimage
+
+# Make syslinux folder to drop in files
+mkdir -p syslinux
+cp $SRC_DIR/sources/syslinux-$SYSLINUX_VERSION/bios/core/isolinux.bin ./syslinux
+cp $SRC_DIR/sources/syslinux-$SYSLINUX_VERSION/bios/com32/elflink/ldlinux/ldlinux.c32 ./syslinux
+echo 'default /kernel.gz initrd=/rootfs.gz' > ./syslinux/isolinux.cfg
+
+# Make the ISO
+genisoimage \
+    -J \
+    -r \
+    -o ../minimal_linux_live.iso \
+    -b syslinux/isolinux.bin \
+    -c boot.cat \
+    -input-charset UTF-8 \
+    -no-emul-boot \
+    -boot-load-size 4 \
+    -boot-info-table \
+    -joliet-long \
+    ./
+cd $SRC_DIR
+set +ex
+
